b14d07873cd95181ea0c460b6414fe284b40e8e2
Steve Romero <romero.steve.a@gmail.com>
2023-06-26 14:47:58 -0700

    simple routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route path="/">
+        <div>
+          Hello Home!!
+        </div>
+      </Route>
+      <Route path="/about">
+        Hello from About
+      </Route>
+      <Route path="/login">
+        Login here
+      </Route>
+    </>
   );
 }
 
