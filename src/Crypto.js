e643c9e4cc8d27b2cc7d40be94360544e049ed00
Steve Romero <romero.steve.a@gmail.com>
2023-06-27 18:36:40 -0700

    showing item price and total balance

--- src/Crypto.js
+++ src/Crypto.js
@@ -6,7 +6,10 @@ function Stock({crypto}) {
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">{crypto.name}</h5>
-          <p className="card-text">{crypto.amount}</p>
+          <p className="card-text">{crypto.amount}:......{crypto.price}   
+          ---------------{crypto.amount*crypto.price}
+          
+          </p>
         </div>
       </div>
     </div>
--- src/profile.js
+++ src/profile.js
@@ -1,76 +1,77 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Crypto from "./Crypto";
 
 function Profile() {
     const [cash, setCash] = useState();
     const [portfolio, setPortfolio] = useState([]);
-    const [value, setValue] = useState("");
 
-    
+
     useEffect(() => {
         fetch(" http://localhost:3001/cash")
-          .then((r) => r.json())
-          .then((s) => setCash(s.amount));
-      }, []);
-
-    useEffect(() => {
-      fetch(" http://localhost:3001/portfolio")
-        .then((r) => r.json())
-        .then((s) => setPortfolio(s));
+            .then((r) => r.json())
+            .then((s) => setCash(s.amount));
     }, []);
 
-    function updateBalance(USD){
-            fetch(" http://localhost:3001/cash",{
-                method:'PATCH',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body:JSON.stringify(USD)
-            })
-              .then((r) => r.json())
-              .then((s) => setCash(s.amount));
+    useEffect(() => {
+        fetch(" http://localhost:3001/portfolio")
+            .then((r) => r.json())
+            .then((s) => setPortfolio(s));
+    }, []);
+
+    function updateCash(USD) {
+        fetch(" http://localhost:3001/cash", {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(USD)
+        })
+            .then((r) => r.json())
+            .then((s) => setCash(s.amount));
     }
 
     function handleSubmit(e) {
         e.preventDefault()
         const val = e.target.value.valueAsNumber;
-        const USD = {amount: 0};
-       
-        
-        if(e.nativeEvent.submitter.id == "deposit"){
+        const USD = { amount: 0 };
+
+
+        if (e.nativeEvent.submitter.id == "deposit") {
             USD.amount = cash + val
-            
-            updateBalance(USD);
+
+            updateCash(USD);
         }
-        else{
-            if (val > cash){
+        else {
+            if (val > cash) {
                 //add error message
                 return;
             }
             USD.amount = cash - val
-            updateBalance(USD);
+            updateCash(USD);
         }
-
-
     }
 
-  return <div>
-    <h1>Profile Page</h1>
-    <form onSubmit={handleSubmit}>
-    <h3>Cash: $ {cash}</h3>
-    <input type="number"  id="value" min="0" value={value}
-     onChange={(e) => setValue(e.target.value)} />
-     <button type="submit" id="deposit">Deposit</button>
-     <button type="submit" id="withdraw">Withdraw</button>
-    
-    </form>
-    
+    //update balance
+    let balance = cash;
+    portfolio.forEach(c => balance += (c.amount * c.price));
 
-    <h2>Crypto</h2>
-      {
-        portfolio.map((c) => (
-          <Crypto key={c.id} crypto={c}  />
-        ))}
+    return <div>
+        <h1>Profile Page</h1>
+        <form onSubmit={handleSubmit}>
+            <h2>Balance: ${balance}</h2>
+            <h3>Cash: $ {cash}</h3>
+            <input type="number" id="value" min="0" />
+            <button type="submit" id="deposit">Deposit</button>
+            <button type="submit" id="withdraw">Withdraw</button>
+
+        </form>
+
+
+        <h2>Crypto</h2>
+        {
+            portfolio.map((c) => (
+                <Crypto key={c.id} crypto={c} />
+            ))}
 
     </div>;
 }
